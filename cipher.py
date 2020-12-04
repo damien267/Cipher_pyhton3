e7586969f085103c0301e0f6f437f6507159291e
Damien Jones <damien226677@gmail.com>
2020-12-04 13:31:31 -0800

    Playing with file I/O and a2b conversion

--- cipher.py
+++ cipher.py
@@ -1,1 +1,26 @@
-//Damien Jones
+#Damien Jones
+
+
+plaintext = open("plaintext.txt", "r+")
+
+
+# Read file here
+if plaintext.mode == 'r+':
+    plaintext_in = plaintext.read()
+
+file_length = len(plaintext_in)
+
+print("Plain text in: " + plaintext_in)
+
+temp = ord('a')
+print("Int a = " + str(temp))
+print("Binary: " + bin(temp))
+
+y = bin(temp)
+
+for i in range(len(y)):
+    if i > 1:
+        print("bin temp i: " + y[i])
+
+
+
